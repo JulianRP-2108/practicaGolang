081cdda1e49c9d25936fcf0feea720a11be533d5
Julian Rodriguez Petz <rodriguezpetzjulian@gmail.com>
2021-09-12 01:11:02 -0300

    primeros endpoints de prueba

--- main/main.go
+++ main/main.go
@@ -1,12 +1,15 @@
 package main
 
 import (
-	"../controllers"
 	"github.com/gin-gonic/gin"
+	"prueba.com/m/controllers"
 )
 
 func main() {
+	gin.SetMode(gin.ReleaseMode)
 	router := gin.Default()
-	router.GET("/", controllers.Ping)
+	router.GET("/personas", controllers.GetAllPersonas)
+	router.POST("/personas", controllers.NewPersona)
 	router.Run()
+	gin.SetMode(gin.ReleaseMode)
 }
